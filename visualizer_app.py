226ac8b05d70799775c0ce7ab31d068e9e9fb8dd
Denis <115725597+Denis-the-second@users.noreply.github.com>
2024-06-28 19:17:00 +0400

    Update visualizer_app.py

--- visualizer_app.py
+++ visualizer_app.py
@@ -23,11 +23,11 @@ def convert_to_df(contents, filename, sheetname):
                 return df
             elif 'xls' in filename:
                 # Assume that the user uploaded an excel file
-                if sheetname != '':
-                    df = pd.read_excel(io.BytesIO(decoded), sheet_name=sheetname)
+                if sheetname == '':
+                    df = pd.read_excel(io.BytesIO(decoded))
                     return df
                 else:
-                    df = pd.read_excel(io.BytesIO(decoded))
+                    df = pd.read_excel(io.BytesIO(decoded), sheet_name=sheetname)
                     return df
         except Exception as e:
             print(e)
